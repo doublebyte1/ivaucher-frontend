1bd264dda25952ca6e135ca0cefaf4ad6f6ac03e
doublebyte1 <info@doublebyte.net>
2021-11-13 19:15:39 +0100

    - Dockerized app - Added nginx server

---
+++ build/precache-manifest.fa334e354931ad5db6d69bdbb01ff2cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e85f85c3f15523b0ce461a0839597eb",
+    "url": "/ivaucher-frontend/index.html"
+  },
+  {
+    "revision": "1af58891ac93b22ebda3",
+    "url": "/ivaucher-frontend/static/css/main.f10095fa.chunk.css"
+  },
+  {
+    "revision": "c1ca898f5dbd846bb3ab",
+    "url": "/ivaucher-frontend/static/js/2.4abfbb62.chunk.js"
+  },
+  {
+    "revision": "1af58891ac93b22ebda3",
+    "url": "/ivaucher-frontend/static/js/main.49e81d03.chunk.js"
+  },
+  {
+    "revision": "82eefcd1aac975c54933",
+    "url": "/ivaucher-frontend/static/js/runtime-main.e7c741d6.js"
+  },
+  {
+    "revision": "e6e77fea42313164f3d19db4d8755b2a",
+    "url": "/ivaucher-frontend/static/media/BR_logo_full_04.e6e77fea.png"
+  }
+]);
