135c4e4eec257c14238f825b5ee5014a574f1a99
doublebyte1 <info@doublebyte.net>
2021-11-13 23:22:40 +0100

    - updated background map

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,7 @@ RUN npm install react-scripts@3.4.1 -g --silent
 
 # add app
 COPY . /
+COPY .env /
 
 # start app
 CMD ["npm", "start"]
--- src/App.js
+++ src/App.js
@@ -105,8 +105,8 @@ const OCD_API_KEY = process.env.REACT_APP_OCD_API_KEY;
       zoom={10}
       >
         <TileLayer
-          url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png"
-          attribution='&copy; Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
       </Map>
       <br></br>
