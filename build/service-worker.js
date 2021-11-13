111025ab5233ef7d622f1dc67be285af8c3e2314
doublebyte1 <info@doublebyte.net>
2021-11-13 17:29:23 +0100

    - optimizations and fixes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ivaucher-frontend/precache-manifest.a73838e518e127940617a4d020907ab7.js"
+  "/ivaucher-frontend/precache-manifest.6d755b0320803942c20016538c0ab440.js"
 );
 
 self.addEventListener('message', (event) => {
